91814962842aca52735e40e76cbced765d4ecd17
Joshua Kovach <kovach.jc@gmail.com>
2014-03-28 10:05:20 -0400

    Configure the library for running on TravisCI
    Removed dummy apps (we load the schema and models
    necessary for testing). Added .travisci.yml with
    a variety of environments to test. Added gemfiles
    for rails 3, 4, and edge.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,34 @@
-# Configure Rails Environment
-ENV["RAILS_ENV"] = "test"
+plugin_test_dir = File.dirname(__FILE__)
 
-dummy_app = ENV[ 'RAILS_VERSION' ] == '3' ? 'dummy_rails3' : 'dummy_rails4'
-
-require File.expand_path("../#{dummy_app}/config/environment.rb",  __FILE__)
+require 'rubygems'
+require 'bundler/setup'
+require 'pry'
+require 'rails'
+require 'active_record'
 require "rails/test_help"
 
-Rails.backtrace_cleaner.remove_silencers!
+require 'logger'
+ActiveRecord::Base.logger = Logger.new(plugin_test_dir + "/debug.log")
 
-# Load support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+require 'yaml'
+require 'erb'
+ActiveRecord::Base.configurations = YAML::load(ERB.new(IO.read(plugin_test_dir + "/db/database.yml")).result)
+ActiveRecord::Base.establish_connection(ENV["DB"] ||= "sqlite3mem")
+ActiveRecord::Migration.verbose = false
 
-# Load fixtures from the engine
-ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
+require 'combustion/database'
+Combustion::Database.create_database(ActiveRecord::Base.configurations[ENV["DB"]])
+load(File.join(plugin_test_dir, "db", "schema.rb"))
+
+require 'activerecord_any_of'
+require 'support/models'
+
+require 'database_cleaner'
+
+ActiveSupport::TestCase.fixture_path = "#{plugin_test_dir}/fixtures"
+ActiveSupport::TestCase.use_transactional_fixtures = true
+ActiveSupport::TestCase.teardown do
+  unless /sqlite/ === ENV['DB']
+    Combustion::Database.drop_database(ActiveRecord::Base.configurations[ENV['DB']])
+  end
+end
