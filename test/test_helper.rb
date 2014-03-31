76be531559ef6d94d678415df886e99362ab5383
Joshua Kovach <kovach.jc@gmail.com>
2014-03-30 23:02:22 -0400

    Initialize dbs before running specs

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,28 +2,30 @@ plugin_test_dir = File.dirname(__FILE__)
 
 require 'rubygems'
 require 'bundler/setup'
-require 'pry'
+
 require 'rails'
 require 'active_record'
+require 'activerecord_any_of'
+
 require "rails/test_help"
+require 'combustion/database'
+require 'database_cleaner'
 
+require 'pry'
 require 'logger'
-ActiveRecord::Base.logger = Logger.new(plugin_test_dir + "/debug.log")
-
 require 'yaml'
 require 'erb'
+
+require 'support/models'
+
+ActiveRecord::Base.logger = Logger.new(plugin_test_dir + "/debug.log")
+
 ActiveRecord::Base.configurations = YAML::load(ERB.new(IO.read(plugin_test_dir + "/db/database.yml")).result)
 ActiveRecord::Base.establish_connection(ENV["DB"] ||= "sqlite3mem")
 ActiveRecord::Migration.verbose = false
 
-require 'combustion/database'
 Combustion::Database.create_database(ActiveRecord::Base.configurations[ENV["DB"]])
 load(File.join(plugin_test_dir, "db", "schema.rb"))
-
-require 'activerecord_any_of'
-require 'support/models'
-
-require 'database_cleaner'
 
 ActiveSupport::TestCase.fixture_path = "#{plugin_test_dir}/fixtures"
 ActiveSupport::TestCase.use_transactional_fixtures = true
